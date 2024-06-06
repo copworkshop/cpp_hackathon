1d0443c862e58b144a5a132147d753f679b62e2a
github-cloudlabsuser-1956 <github_cloudlabsuser_1956@cloudlabsaiuser.com>
2024-06-06 12:35:31 +0000

    add /db

--- PWServer/main.cpp
+++ PWServer/main.cpp
@@ -1,39 +1,51 @@
 #include <iostream>
+#include <fstream>
+#include <nlohmann/json.hpp>
 
 #include <PWServer.h>
 #include <PWException.h>
 
 int main(int argc, char* argv[])
 {
-	std::string infile,outfile;
-	if (argc != 5) {
-		std::cerr << "Usage: " <<argv[0]<< " /infile <filename> /outfile <filename>"<<std::endl;
-		return 1;
-	}
-	for (int i = 1; i < argc; i += 2) {
+    std::string infile, outfile, dbfile;
+    if (argc != 5 && argc != 7) {
+        std::cerr << "Usage: " <<argv[0]<< " /infile <filename> /outfile <filename> [/db <dbfile>]"<<std::endl;
+        return 1;
+    }
+    for (int i = 1; i < argc; i += 2) {
         std::string argName = argv[i];
         if (argName == "/infile") {
             infile = argv[i + 1];
         } else if (argName == "/outfile") {
             outfile = argv[i + 1];
+        } else if (argName == "/db") {
+            dbfile = argv[i + 1];
         } else {
             std::cerr << "Unknown argument: " << argName << std::endl;
             return 1;
         }
     }
 
-	try
-	{
+    try
+    {
+        // if (!dbfile.empty()) {
+        //     // Read database configuration from file
+        //     std::ifstream dbConfigFile(dbfile);
+        //     nlohmann::json dbConfig;
+        //     dbConfigFile >> dbConfig;
 
-
-		PWServer server(infile, outfile);
-		server.Start();
-	}
-	catch (const PWException& exp)
-	{
-		std::cout << exp.what();
-		return -1;
-	}
+        //     PWServer server(infile, outfile, dbConfig);
+        //     server.Start();
+        // } else {
+            PWServer server(infile, outfile);
+            server.Start();
+        // }
+    }
+    catch (const PWException& exp)
+    {
+        std::cout << exp.what();
+        return -1;
+    }
 
     return 0;
 }
