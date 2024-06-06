b597262e7a398170045f1971e09ca6812e28b5e7
github-cloudlabsuser-2022 <github_cloudlabsuser_2022@cloudlabsaiuser.com>
2024-06-06 10:46:39 +0000

    increase coverage

--- PWTests/PWTests.cpp
+++ PWTests/PWTests.cpp
@@ -48,3 +48,39 @@ TEST(PWInventory, TestUpdateQualityZeroQuality) {
     inventory.UpdateQuality();
     ASSERT_EQ(inventory[0].value, 0);
 }
+
+// Test updating quality for an item with positive sellIn and positive quality
+TEST(PWInventory, TestUpdateQualityPositiveSellInPositiveQuality) {
+	PWInventory inventory{ { InventoryItem{ "Foo", 5 , 10 } } };
+	inventory.UpdateQuality();
+	ASSERT_GT(inventory[0].value, 0);
+	ASSERT_LT(inventory[0].value, 10);
+}
+
+// Test updating quality for an item with positive sellIn and maximum quality
+TEST(PWInventory, TestUpdateQualityPositiveSellInMaxQuality) {
+	PWInventory inventory{ { InventoryItem{ "Foo", 5 , 50 } } };
+	inventory.UpdateQuality();
+	ASSERT_EQ(inventory[0].value, 49);
+}
+
+// Test updating quality for an item with zero sellIn and positive quality
+TEST(PWInventory, TestUpdateQualityZeroSellInPositiveQuality) {
+	PWInventory inventory{ { InventoryItem{ "Foo", 0 , 10 } } };
+	inventory.UpdateQuality();
+	ASSERT_LT(inventory[0].value, 10);
+}
+
+// Test updating quality for an item with zero sellIn and maximum quality
+TEST(PWInventory, TestUpdateQualityZeroSellInMaxQuality) {
+	PWInventory inventory{ { InventoryItem{ "Foo", 0 , 50 } } };
+	inventory.UpdateQuality();
+	ASSERT_EQ(inventory[0].value, 48);
+}
+
+// Test updating quality for an item with negative sellIn and maximum quality
+TEST(PWInventory, TestUpdateQualityNegativeSellInMaxQuality) {
+	PWInventory inventory{ { InventoryItem{ "Foo", -1 , 50 } } };
+	inventory.UpdateQuality();
+	ASSERT_EQ(inventory[0].value, 48);
+}
