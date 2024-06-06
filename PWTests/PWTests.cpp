2c24050b5aa76a185595cbedbba1cb34c5b67d8c
github-cloudlabsuser-2104 <github_cloudlabsuser_2104@cloudlabsaiuser.com>
2024-06-06 10:01:50 +0000

    PWServer UT - exception throw

--- PWTests/PWTests.cpp
+++ PWTests/PWTests.cpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 #include <PWInventory.h>
-
+#include <PWServer.h>
 // TIP: Keep the tests simple, aim for good coverage 
 // TEST(PWInventory, FooBad) {
 // 	PWInventory inventory{ { InventoryItem{ "Foo", 0 , 0 } } };
@@ -9,4 +9,15 @@
 TEST(PWInventory, FooGood) {
 	PWInventory inventory{ { InventoryItem{ "Foo", 0 , 0 } } };
 	EXPECT_EQ(inventory[0].name, "Foo");
+}
+
+TEST(PWServerTest, shouldThrowIfFileCantBeOpened)
+{
+    // Arrange
+    std::string invFile = "input_inventory.txt";
+    std::string outFile = "output_file.txt";
+    PWServer server(invFile, outFile);
+
+    // Act
+	EXPECT_THROW(server.Start(), std::exception);
 }
