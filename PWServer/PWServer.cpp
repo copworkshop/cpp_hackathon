d1ea3528c0d0b3b8ea8b161db29259ab084cc9a5
github-cloudlabsuser-1956 <github_cloudlabsuser_1956@cloudlabsaiuser.com>
2024-06-06 13:53:30 +0000

    add mysql

--- PWServer/PWServer.cpp
+++ PWServer/PWServer.cpp
@@ -3,24 +3,47 @@
 
 #include <iostream>
 #include <PWException.h>
+#include <nlohmann/json.hpp>
 
-PWServer::PWServer(const std::string& invFile,
-				  const std::string& outFile)
-	: invFile(invFile),outFile(outFile)
+PWServer::PWServer(const std::string& invFile, const std::string& outFile)
+    : invFile(invFile), outFile(outFile), useDatabase(false)
 {
 }
 
-void PWServer::Start()
+PWServer::PWServer(const std::string& invFile, const std::string& outFile, const nlohmann::json& dbConfig)
+    : invFile(invFile), outFile(outFile), dbConfig(dbConfig), useDatabase(true)
 {
-	//Init inventory
-	// TODO: Refactor to allow mock injection, but PWInventory has operator[] function
-	// which is not supported by gmock to be mocked so it needs another refactor.
-	std::unique_ptr<PWInventory> inventory = std::make_unique<PWInventory>();
-	inventory->Load(invFile);
+    connectToDatabase();
+}
 
-	//Update the inventory
-	inventory->UpdateQuality();
+void PWServer::connectToDatabase() {
+    con = mysql_init(NULL);
+    if (!mysql_real_connect(con, dbConfig["host"].get<std::string>().c_str(), dbConfig["user"].get<std::string>().c_str(), dbConfig["password"].get<std::string>().c_str(), dbConfig["database"].get<std::string>().c_str(), 0, NULL, 0)) {
+        std::cerr << mysql_error(con) << std::endl;
+        exit(1);
+    }
+}
 
-    // Store the inventory
-	inventory->Save(outFile);
+void PWServer::readFromDatabase() {
+    // Implement logic to read data from the database
+}
+
+void PWServer::writeToDatabase() {
+    // Implement logic to write data to the database
+}
+
+void PWServer::Start() {
+    std::unique_ptr<PWInventory> inventory = std::make_unique<PWInventory>();
+
+    if (useDatabase) {
+        readFromDatabase();
+        // Update the inventory
+        inventory->UpdateQuality();
+        writeToDatabase();
+    } else {
+        inventory->Load(invFile);
+        // Update the inventory
+        inventory->UpdateQuality();
+        inventory->Save(outFile);
+    }
 }
--- PWServer/PWServer.h
+++ PWServer/PWServer.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <string>
+#include <string>
+#include <nlohmann/json.hpp>
+#include <mysql.h>
 
 /**
  * @class PWServer
@@ -11,6 +14,12 @@ class PWServer
 private:
     const std::string& invFile; /**< The path to the input inventory file. */
     const std::string& outFile; /**< The path to the output file. */
+        void connectToDatabase();
+    void readFromDatabase();
+    void writeToDatabase();
+    nlohmann::json dbConfig;
+    bool useDatabase;
+    MYSQL *con;
 
 public:
     /**
@@ -19,6 +28,8 @@ public:
      * @param outFile The path to the output file.
      */
     PWServer(const std::string& invFile, const std::string& outFile);
+    PWServer(const std::string& invFile, const std::string& outFile, const nlohmann::json& dbConfig);
+ 
 
     /**
      * @brief Starts the server and begins processing the inventory file.
