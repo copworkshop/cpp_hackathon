9351b4d4f852815266ed876327c80d26bd0d8cbe
Wojtek <github_cloudlabsuser_1966@cloudlabsaiuser.com>
2024-06-06 10:23:20 +0000

    Added simple UTs for PWInventory class.

--- PWTests/PWTests.cpp
+++ PWTests/PWTests.cpp
@@ -2,11 +2,35 @@
 #include <PWInventory.h>
 
 // TIP: Keep the tests simple, aim for good coverage 
-// TEST(PWInventory, FooBad) {
-// 	PWInventory inventory{ { InventoryItem{ "Foo", 0 , 0 } } };
-// 	EXPECT_EQ(inventory[0].name, "Bar");
-// }
+
+TEST(PWInventory, Save) {
+	PWInventory inventory{ { InventoryItem{ "Foo", 0 , 0 } } };
+	inventory.Save("test.json");
+}
+
+TEST(PWInventory, Load) {
+	PWInventory inventory;
+	inventory.Load("test.json");
+}
+
+TEST(PWInventory, UpdateQuality) {
+	PWInventory inventory{ { InventoryItem{ "Foo", 0 , 0 } } };
+	inventory.UpdateQuality();
+}
+
+TEST(PWInventory, Operator) {
+	PWInventory inventory{ { InventoryItem{ "Foo", 0 , 0 } } };
+	InventoryItem item = inventory[0];
+	EXPECT_EQ(item.name, "Foo");
+}
+
 TEST(PWInventory, FooGood) {
 	PWInventory inventory{ { InventoryItem{ "Foo", 0 , 0 } } };
 	EXPECT_EQ(inventory[0].name, "Foo");
 }
+
+TEST(PWInventory, Count) {
+	PWInventory inventory{ { InventoryItem{ "Foo", 0 , 0 } } };
+	EXPECT_EQ(inventory.Count(), 1);
+}
+
